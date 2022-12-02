e57ca0b2a9601f77286a33e860708e9d7843098b
Mario Finelli <mario@finel.li>
2022-12-02 11:54:01 +0100

    Add license

--- src/dayone.rs
+++ src/dayone.rs
@@ -1,3 +1,18 @@
+/* Copyright 2022 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 //! Advent of Code 2022 Day 1: <https://adventofcode.com/2022/day/1>
 //!
 //! This challenge essentially boils down to getting the `n` largest items,
