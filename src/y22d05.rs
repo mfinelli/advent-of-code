4ebdbb4bf35a2297936cb57d8940d16f5e699615
Mario Finelli <mario@finel.li>
2022-12-05 17:03:48 +0100

    Cleanup day five solutions

--- src/y22d05.rs
+++ src/y22d05.rs
@@ -13,15 +13,68 @@
  * limitations under the License.
  */
 
+//! Advent of Code 2022 Day 5: <https://adventofcode.com/2022/day/5>
+//!
+//! The day five challenge mostly revolves around vector manipulation (my
+//! chosen methodology for managing the state of the crates). The most
+//! challenging part was to parse the input text into the initial state
+//! representation.
+//!
+//! Once the input has been parsed part one has us move crates one at a time
+//! from the top (end) of one stack onto another i.e., [`std::vec::Vec::pop`]
+//! from one vector and then [`std::vec::Vec::push`] onto another. The second
+//! part requires maintaining the order of multiple crates as they are moved
+//! from one stack to another, so we just move them one at a time into a
+//! temporary vector, reverse the order of that and then play them back into
+//! the destination vector.
+
+/// The solution for the day five challenge.
+///
+/// As the arguments correspond to the input text and the method of operation:
+/// the reordering of crates (part `1`) or the maintaining of the order (part
+/// `2`).
+///
+/// # Parsing the Initial State
+/// To parse the initial state we first need to get all of the lines before
+/// the first empty line which we can treat as the delimiter between the state
+/// representation and the moves that we will need to make. The last non-empty
+/// line of the state representation gives us a numbering of the columns
+/// (which we can use to determine the total number of columns) that needs to
+/// be discarded as it is not actually a part of the internal state
+/// representation. Once we have the lines that represent the state we can
+/// loop through them (in reverse order so that the items on the bottom of the
+/// stacks are the first items in the vector) checking each column to see if
+/// there is an element defined there or not (e.g, empty space or end-of-input)
+/// and if there is then add it to the appropriate column vector.
+///
+/// # Example
+/// ```rust
+/// # use aoc::y22d05::y22d05;
+/// // probably read this in from the input file...
+/// let input = concat![
+///     "    [D]    \n",
+///     "[N] [C]    \n",
+///     "[Z] [M] [P]\n",
+///     " 1   2   3 \n",
+///     "\n",
+///     "move 1 from 2 to 1\n",
+///     "move 3 from 1 to 3\n",
+///     "move 2 from 2 to 1\n",
+///     "move 1 from 1 to 2\n",
+/// ];
+/// assert_eq!(y22d05(&input, 1), "CMZ");
+/// assert_eq!(y22d05(&input, 2), "MCD");
+/// ```
 pub fn y22d05(input: &str, part: u32) -> String {
     let lines: Vec<_> = input.lines().collect();
 
     let mut state = parse_initial_state(&lines);
-    let mut in_moves = false;
+    let mut in_moves = false; // track if we've hit the state/moves separator
     let mut output = String::new();
 
     for line in lines {
         if in_moves {
+            // "move X from Y to Z": X is index 0, Y is index 3, Z is index 5
             let text: Vec<&str> = line.split_whitespace().collect();
             let how_many_to_move = text[1].parse().unwrap();
             let from_index: u32 = text[3].parse().unwrap();
@@ -51,26 +104,35 @@ pub fn y22d05(input: &str, part: u32) -> String {
                 }
             }
         } else if line.is_empty() {
+            // we've hit the empty line, switch "modes"
             in_moves = true;
-        }
+        } // else continue
     }
 
     for mut stack in state {
         // TODO: add test for empty column
-        output += &stack.pop().unwrap();
+        output += match &stack.pop() {
+            Some(item) => item,
+            None => " ",
+        };
     }
 
     output
 }
 
+/// The process of processing the initial state is described in [`y22d05`]
+/// under the "Parsing the Initial State" header.
 fn parse_initial_state(lines: &Vec<&str>) -> Vec<Vec<String>> {
-    // let mut state_lines: Vec<&str> = Vec::Vec<&str>::new();
     let mut state_lines: Vec<&str> = Vec::new();
     let mut state: Vec<Vec<String>> = Vec::new();
 
     for line in lines {
+        // once we find the empty line we _remove_ (pop) the previous line
+        // that we saw because it's not actually part of the state
+        // representation and then we parse it to figure out how many columns
+        // we should have and then create the necessary empty vectors for each
+        // stack.
         if line.is_empty() {
-            // state_lines.remove(state_lines.len() - 1);
             let columns = state_lines.pop().unwrap();
             let number_of_columns: u32 =
                 columns.split_whitespace().last().unwrap().parse().unwrap();
@@ -82,23 +144,34 @@ fn parse_initial_state(lines: &Vec<&str>) -> Vec<Vec<String>> {
             break;
         }
 
+        // we haven't hit the empty line that splits the state and moves yet
+        // so add this line to the "state_lines" that we'll process below
         state_lines.push(line);
     }
 
+    // loop through the lines that represent the state in reverse order
+    // (meaning that we start at the _bottom_ of the columns so that we can
+    // add each element to the column vector
     for line in state_lines.iter().rev() {
-        let number_of_columns = (line.len() + 1) / 4;
-        let mut column = 0;
-        for i in 0..number_of_columns {
+        // whitespace doesn't extend to the end of each line for all columns
+        // so we need to calculate how many actual columns are in a given line
+        //
+        // columns are represented by a length of 4, we add one to the line
+        // length to compensate for the column spacer trimmed at the end of
+        // the line
+        let number_of_columns_in_line = (line.len() + 1) / 4;
+
+        // loop through the columns, and if a column begins with a "["
+        // character it means that the column contains an element, add it to
+        // the current column
+        for (i, column) in
+            state.iter_mut().enumerate().take(number_of_columns_in_line)
+        {
             if line.chars().nth(i * 4).unwrap() == '[' {
-                state[column]
-                    .push(line.chars().nth(i * 4 + 1).unwrap().to_string());
+                column.push(line.chars().nth(i * 4 + 1).unwrap().to_string());
             }
-            column += 1;
         }
     }
-
-    // println!("{:?}", state_lines);
-    // println!("{:?}", state);
 
     state
 }
@@ -131,6 +204,20 @@ mod tests {
                 vec!["K"],
             ]
         );
+    }
+
+    #[test]
+    fn it_works() {
+        let input = concat!(
+            "[A]\n",
+            "[B] [C] [D]\n",
+            " 1   2   3\n",
+            "\n",
+            "move 2 from 1 to 2\n",
+        );
+
+        assert_eq!(y22d05(input, 1), " BD");
+        assert_eq!(y22d05(input, 2), " AD");
     }
 
     #[test]
