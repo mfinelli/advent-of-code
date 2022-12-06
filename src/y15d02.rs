a7fe8911b6adf3160d0e4937928be9c76f187add
Mario Finelli <mario@finel.li>
2022-12-06 19:35:32 +0100

    Cleanup day two solutions

--- src/y15d02.rs
+++ src/y15d02.rs
@@ -1,3 +1,68 @@
+/* Copyright 2022 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Advent of Code 2015 Day 2: <https://adventofcode.com/2015/day/2>
+//!
+//! The solutions today are simple and just involve parsing numbers and
+//! comparing them amongst each other. Originally this worked with some ugly
+//! if/else statments and array sorting, but after solving the puzzle I
+//! decided to switch it to use a [min
+//! heap](https://en.wikipedia.org/wiki/Min-max_heap) which I think results
+//! in a much cleaner and easier to understand/reason about solution.
+
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+/// The solution for the day two challenge.
+///
+/// Given the input as a string we start by parsing all of the dimensions on
+/// each line. Then if we're doing part `1` we calculate the area of each
+/// side, add them to our heap and then pop the smallest value back. The
+/// actual calculation comes directly from the challenge prompt:
+/// `2*l*w + 2*w*h + 2*h*l` plus the extra (area of the smallest side that we
+/// just got).
+///
+/// If we're doing part `2` then we calculate the cubic volume (`l * w * h`),
+/// and then find the smallest two sides by adding the dimensions to the heap
+/// and then popping the smallest two values back. The calculation again
+/// comes directly from the prompt and is equal to the diameter of the
+/// smallest side (two times the smallest two dimensions each) plus the volume
+/// previously calculated.
+///
+/// # Example
+/// ```rust
+/// # use aoc::y15d02::y15d02;
+/// let input = "1x2x3\n4x5x6\n"; // probably read this from the input file...
+/// assert_eq!(y15d02(input, 1), 192);
+/// assert_eq!(y15d02(input, 2), 150);
+/// ```
+///
+/// # Min-Heap values
+/// The rust documentation isn't exactly clear about how to work with a
+/// min-heap which uses the same data structure as the max heap, but this
+/// helpful answer on [stack overflow](https://stackoverflow.com/a/71121846)
+/// provides the answer, reproduced here:
+///
+/// ```rust
+/// # use std::cmp::Reverse;
+/// let reversed = Reverse(42);
+/// assert_eq!(reversed.0, 42);
+/// // alternatively, using destructuring:
+/// let Reverse(n) = reversed;
+/// assert_eq!(n, 42);
+/// ```
 pub fn y15d02(input: &str, part: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
     let mut total = 0;
@@ -13,29 +78,20 @@ pub fn y15d02(input: &str, part: u32) -> u32 {
             let wh = w * h;
             let hl = h * l;
 
-            let extra = if lw <= wh && lw <= hl {
-                lw
-            } else if wh <= lw && wh <= hl {
-                wh
-            } else {
-                hl
-            };
+            let mut heap =
+                BinaryHeap::from([Reverse(lw), Reverse(wh), Reverse(hl)]);
+            let Reverse(extra) = heap.pop().unwrap();
 
             total += 2 * lw + 2 * wh + 2 * hl + extra;
         } else {
             let volume = l * w * h;
-            let mut sides = [l, w, h];
-            sides.sort();
+            let mut heap =
+                BinaryHeap::from([Reverse(l), Reverse(w), Reverse(h)]);
 
-            for (i, side) in sides.iter().enumerate() {
-                total += 2 * side;
+            let Reverse(s1) = heap.pop().unwrap();
+            let Reverse(s2) = heap.pop().unwrap();
 
-                if i == 1 {
-                    break;
-                }
-            }
-
-            total += volume;
+            total += 2 * s1 + 2 * s2 + volume;
         }
     }
 
