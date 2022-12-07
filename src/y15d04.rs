fc06ef895494e8b56704648d0bd6810a45fbeb07
Mario Finelli <mario@finel.li>
2022-12-07 01:36:30 +0100

    Solve day four part two

--- src/y15d04.rs
+++ src/y15d04.rs
@@ -1,12 +1,11 @@
 use md5::{Md5, Digest};
 
-pub fn y15d04(input: &str) -> Option<u64> {
+pub fn y15d04(input: &str, leading_zeros: u32) -> Option<u64> {
+    let check = "0".repeat(leading_zeros as usize);
     let bytes = input.trim().as_bytes();
-    // println!("{}", hash.get(0..5).unwrap());
     for i in 1..u64::MAX {
-        // let hash = format!("{:x}", Md5::digest(trimmed + &i.to_string()));
         let hash = format!("{:x}", Md5::new().chain_update(bytes).chain_update(i.to_string().as_bytes()).finalize());
-        if hash.get(0..5).unwrap() == "00000" {
+        if hash.get(0..leading_zeros as usize).unwrap() == check {
             return Some(i);
         }
     }
@@ -22,16 +21,17 @@ mod tests {
     #[test]
     fn it_works() {
         let mut input = "abcdef\n";
-        assert_eq!(y15d04(input), Some(609043));
+        assert_eq!(y15d04(input, 5), Some(609043));
 
         input = "pqrstuv";
-        assert_eq!(y15d04(input), Some(1048970));
+        assert_eq!(y15d04(input, 5), Some(1048970));
     }
 
     #[test]
     fn the_solution() {
         let contents = fs::read_to_string("input/2015/day04.txt").unwrap();
 
-        assert_eq!(y15d04(&contents).unwrap(), 254575);
+        assert_eq!(y15d04(&contents, 5).unwrap(), 254575);
+        assert_eq!(y15d04(&contents, 6).unwrap(), 1038736);
     }
 }
