abe9cd05c69dbd6251df40025b3f3685513b194e
Mario Finelli <mario@finel.li>
2022-12-11 18:43:05 +0100

    Cleanup day eleven solutions

--- src/y22d11.rs
+++ src/y22d11.rs
@@ -14,9 +14,63 @@
  */
 
 //! Advent of Code 2022 Day 11: <https://adventofcode.com/2022/day/11>
+//!
+//! I thought that part one of this challenge was relatively easy and was at
+//! first surprised that part two was just to increase the number of rounds
+//! and remove a division step. Then I ran into my first overflow, increased
+//! my integers to `u64` and then `u128` and they were still too small. I
+//! thought that the prompt's instructions "you'll need to find another way to
+//! keep your worry levels manageable" was just flavor text. I started to
+//! implement a crate that added support for `u256`+ sized integers until I
+//! decided that I was missing something. I wasn't really sure how to approach
+//! the problem until I read something really helpful on
+//! [reddit](https://www.reddit.com/r/adventofcode/comments/zim5o6/comment/izrr6mq/):
+//!
+//! >Hint: you don't care what the result of the division is, you just care
+//! >whether the current worry level is divisible by the monkey's test value.
+//! >Is there a way you can keep the worry level small(er) while still being
+//! >able to tell if it's divisible?
+//!
+//! I played around some trying to divide the original factor until I found
+//! the modulo method. During my cleanup phase I also read that it's better to
+//! compute the least common multiple and use that instead of the product, but
+//! reading around some more it seems that may be bad advice. I already
+//! implemented it again using the LCM and the tests still pass so it can't
+//! _hurt_, but I don't understand the math well enough to know for sure one
+//! way or the other.
+//!
+//! What follows is my attempt to explain why/how the modulo method is needed
+//! and works. Inspecting the monkeys we can see that some (or rather one) of
+//! them multiplies the item value by itself (i.e., `value^2`) each time that
+//! it inspects an object (it must be _really_ careless). In part one we have
+//! a low enough number of rounds that reducing the worry factor by three each
+//! time is enough to keep this value from getting too out of hand. It also
+//! probably wouldn't be a problem if we never had the exponential growth and
+//! only multiplied (or added) by set, fixed values. In any case, as it is the
+//! item values can grow crazy big crazy fast. The strategy then is to compute
+//! a value that we can remove from the item's value that will reduce its size
+//! but keep in tact whether it's divisible by all of the monkeys' test values.
+//! Consider some smaller, arbitrary numbers for the tests: `3`, `5`, and `7`.
+//! Let's assume that we have an item with value `300` and that our overflow
+//! is small say `500`. If we have a monkey that multiplies the item value by
+//! itself we'll end up with a worry score of 90000 which would obviously
+//! overflow. We also want to know whether the result is divisible by our
+//! initial numbers and if not what the remainder is. `90000 % 3` and `90000 %
+//! 5` are both `0`, but `90000 % 7` is `1`. We can "take out" `857` copies of
+//! our LCM `105` (`857 * 105 == 89985`) and be left with `15`. `15 % 3` and
+//! `15 % 5` are both `0` and `15 % 7` is `1`, just like our original, huge
+//! number. We can safely replace that value with `15` which fits inside our
+//! overflow. There are some much better, math filled explanations on the
+//! Advent of Code subreddit that explain why/how this all works should you be
+//! interested.
 
+use crate::util;
 use std::collections::VecDeque;
 
+/// OperationType is a representation of how the worry level changes, we can
+/// either add/multiply by another value or add/multiply the same value. This
+/// lets store the strategy for each monkey in a way that will let us `match`
+/// later.
 #[derive(Debug)]
 enum OperationType {
     Addition,
@@ -25,6 +79,11 @@ enum OperationType {
     MultiplicationSelf,
 }
 
+/// Monkey is the current state of each monkey, how many item inspections that
+/// they have done, which items they're currently holding, which worry level
+/// strategy they implement (and the value modifier if it's not *Self), as
+/// well as the information necessary for determining how and to which monkey
+/// it will pass items.
 #[derive(Debug)]
 struct Monkey {
     inspections: u64,
@@ -38,10 +97,75 @@ struct Monkey {
 
 /// The solution for the day eleven challenge.
 ///
+/// There are three input arguments: the input (monkey definitions) as a
+/// string, the number of rounds to simulate as an integer, and a boolean
+/// whether or not we should implement the "static relief" method (i.e., divide
+/// the worry by `3` on each inspection). If it's `false` then we do part two
+/// of the challenge where we reduce the worry using the modulo method.
+///
+/// We start by parsing the input. Each monkey's definition is the same: it
+/// takes six lines (seven including a trailing newline, so we split the input
+/// into chunks of seven lines to parse it). The first line is the monkey's
+/// "name" or index, starting with `0`. The next line is a comma-separated
+/// list of the items/worry that a monkey is currently holding. The next line
+/// is the definition of how the worry changes each time the monkey inspects
+/// an item. There are four possibilities: the monkey can add or multiply by
+/// a static number or they monkey can add the item's value to itself or
+/// multiply the item's value by itself. Finally, are three lines that tell
+/// the monkey where to send the item next. The first of these three lines is
+/// the number to divide the item by and if the result is `0` (i.e., no
+/// remainder) then the monkey gives the item to the monkey on the second line
+/// but otherwise (i.e., there _is_ a remainder) then the monkey gives the
+/// item to the monkey on the third line.
+///
+/// Parsing this input essentially amounts to splitting a bunch of whitespace
+/// and parsing the integers found in certain indexes of the result. One
+/// important implementation note is that the monkeys inspect the items in a
+/// FIFO (first-in-first-out) order meaning that a standard vector is
+/// unsuitable for tracking the items that the monkey is holding. We instead
+/// use a [`std::collections::VecDeque`] and its `push_back()` and
+/// `pop_front()` methods to achieve this behavior.
+///
+/// Then we start simulating the rounds. We check each item that the monkey
+/// is holding (`pop()`ping them from the items list) and apply the worry
+/// modification to it. We increase the number of inspections that the monkey
+/// has done and then apply a worry relief strategy. We then check if the
+/// item is divisible by the monkey's `test` number and then pass the item to
+/// the corresponding monkey.
+///
+/// Finally, we sort the monkeys by how many inspections they did and return
+/// the number of inspections made by the top two monkeys multiplied together.
+///
 /// # Example
 /// ```rust
+/// # use aoc::y22d11::y22d11;
+/// // probably read this from the input file...
+/// let input = concat!(
+///     "Monkey 0:\n",
+///     "  Starting items: 1, 2\n",
+///     "  Operation: new = old * 8\n",
+///     "  Test: divisible by 3\n",
+///     "    If true: throw to monkey 2\n",
+///     "    If false: throw to monkey 1\n",
+///     "\n",
+///     "Monkey 1:\n",
+///     "  Starting items: 3, 4, 5, 6\n",
+///     "  Operation: new = old + 9\n",
+///     "  Test: divisible by 5\n",
+///     "    If true: throw to monkey 0\n",
+///     "    If false: throw to monkey 2\n",
+///     "\n",
+///     "Monkey 2:\n",
+///     "  Starting items: 7\n",
+///     "  Operation: new = old * old\n",
+///     "  Test: divisible by 7\n",
+///     "    If true: throw to monkey 1\n",
+///     "    If false: throw to monkey 0",
+/// );
+/// assert_eq!(y22d11(&input, 5, true), 858);
+/// assert_eq!(y22d11(&input, 5, false), 896);
 /// ```
-pub fn y22d11(input: &str, rounds: u32, relief: bool) -> u64 {
+pub fn y22d11(input: &str, rounds: u32, static_relief: bool) -> u64 {
     let lines: Vec<_> = input.lines().collect();
     let mut monkeys = Vec::new();
 
@@ -96,7 +220,7 @@ pub fn y22d11(input: &str, rounds: u32, relief: bool) -> u64 {
 
     let mut lcm = 1;
     for monkey in &monkeys {
-        lcm *= u64::from(monkey.test);
+        lcm = util::lcm(lcm, u64::from(monkey.test));
     }
 
     for _ in 0..rounds {
@@ -114,7 +238,7 @@ pub fn y22d11(input: &str, rounds: u32, relief: bool) -> u64 {
                         item +=
                             u64::from(monkeys[monkey_index].operation_value);
                     }
-                    OperationType::MultiplicationSelf => item = item * item,
+                    OperationType::MultiplicationSelf => item *= item,
                     OperationType::Multiplication => {
                         item *=
                             u64::from(monkeys[monkey_index].operation_value);
@@ -124,15 +248,14 @@ pub fn y22d11(input: &str, rounds: u32, relief: bool) -> u64 {
                 // increment the monkey's inspection counter
                 monkeys[monkey_index].inspections += 1;
 
-                if relief {
+                // implement the relief algorithm
+                if static_relief {
                     item /= 3;
-                } else {
-                    if item > lcm {
-                        item %= lcm;
-                    }
+                } else if item > lcm {
+                    item %= lcm;
                 }
 
-                // finally throw (assign) the item to a new monkey
+                // finally, throw (assign) the item to a new monkey
                 if item % u64::from(monkeys[monkey_index].test) == 0 {
                     let new_monkey_index = monkeys[monkey_index].if_true;
                     monkeys[new_monkey_index].items.push_back(item);
