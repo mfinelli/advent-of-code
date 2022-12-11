9a2c8ea0f6a91c633be598e624229e9edde7ecd7
Mario Finelli <mario@finel.li>
2022-12-12 00:23:50 +0100

    Fix style for day five challenge

--- src/y15d05.rs
+++ src/y15d05.rs
@@ -1,8 +1,24 @@
+/* Copyright 2022 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Advent of Code 2015 Day 5: <https://adventofcode.com/2015/day/5>
+
+/// The solution for the day five challenge.
 pub fn y15d05(input: &str, part: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
     let mut nice_strings = 0;
-
-    // let mut tmp = 0;
 
     for line in lines {
         if part == 1 && is_nice_p1(line) {
@@ -10,11 +26,6 @@ pub fn y15d05(input: &str, part: u32) -> u32 {
         } else if part == 2 && is_nice_p2(line) {
             nice_strings += 1;
         }
-
-        // if tmp == 1 {
-        //     break;
-        // }
-        // tmp += 1;
     }
 
     nice_strings
@@ -37,7 +48,7 @@ fn is_nice_p1(s: &str) -> bool {
         }
 
         if i < chars.len() - 1 {
-            if c == &chars[i+1] {
+            if c == &chars[i + 1] {
                 double = true;
             }
         }
@@ -51,46 +62,24 @@ fn is_nice_p1(s: &str) -> bool {
 }
 
 fn is_nice_p2(s: &str) -> bool {
-    println!("{}", s);
     let chars: Vec<_> = s.chars().collect();
 
     let mut non_overlapping_pair = false;
     let mut repeat_with_middle = false;
-    // println!("{}", chars.len());
     for i in 0..chars.len() {
-        // if i < chars.len() - 2 {
-        //     if chars[i] == chars[i+2] && chars[i+1] == chars[i+3] {
-        //         non_overlapping_pair = true;
-        //     }
-            // for (j, c) in chars.iter().enumerate() {
-            // for j in 0..chars.len() -1 {
-            //     if j != i && j + 1 != i {
-            //         if chars[i] == chars[j] && chars[i+1] == chars[j+1] {
-            //             non_overlapping_pair = true;
-            //         }
-            //     }
-            // }
-            // println!("{:?}", chars.windows(2));
-            if i != chars.len() - 1{
-            // println!("{}/{}: {}{}", s, i, chars[i], chars[i+1]);
+        if i != chars.len() - 1 {
             for (j, window) in chars.windows(2).enumerate() {
-                if i == j || i + 1 == j || (i >= 1 && i - 1 == j){
-                    // println!("discarding window {}", window.iter().collect::<String>());
+                if i == j || i + 1 == j || (i >= 1 && i - 1 == j) {
                     continue;
-                } else {
-                    // println!("checking window {}", window.iter().collect::<String>());
                 }
 
                 if chars[i] == window[0] && chars[i + 1] == window[1] {
                     non_overlapping_pair = true;
-                    // println!("found non_overlapping_pair {} {}, {} {}", s, window.iter().collect::<String>(), i, j);
                 }
-                // println!("{:?}", window);
-                // println!("{}: {}", j, window.iter().collect::<String>());
             }
         }
 
-        if i < chars.len() - 2 && chars[i] == chars[i+2] {
+        if i < chars.len() - 2 && chars[i] == chars[i + 2] {
             repeat_with_middle = true;
         }
 
