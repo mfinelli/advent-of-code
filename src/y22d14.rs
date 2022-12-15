b715bb04f2989c245293fef19257e8dcb008039e
Mario Finelli <mario@finel.li>
2022-12-15 21:35:59 +0100

    Do initial cleanup for day fourteen

--- src/y22d14.rs
+++ src/y22d14.rs
@@ -1,5 +1,36 @@
+/* Copyright 2022 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Advent of Code 2022 Day 14: <https://adventofcode.com/2022/day/14>
+//!
+//! TODO
+
 use std::collections::{BinaryHeap, HashSet};
 
+/// The solution for the day fourteen challenge.
+///
+/// TODO
+///
+/// # Example
+/// ```rust
+/// # use aoc::y22d14::y22d14;
+/// // probably read this from the input file...
+/// let input = "499,4 -> 499,5 -> 496,5\n503,4 -> 502,4 -> 502,7 -> 494,7";
+/// assert_eq!(y22d14(input, 1), 19);
+/// assert_eq!(y22d14(input, 2), 54);
+/// ```
 pub fn y22d14(input: &str, part: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
     let mut occupied = HashSet::new();
@@ -19,22 +50,18 @@ pub fn y22d14(input: &str, part: u32) -> u32 {
             }
 
             let coordinates: Vec<_> = part.split(',').collect();
-            let mut end_x: usize = coordinates[0].parse().unwrap();
-            let mut end_y: usize = coordinates[1].parse().unwrap();
+            let end_x: usize = coordinates[0].parse().unwrap();
+            let end_y: usize = coordinates[1].parse().unwrap();
             lowest.push(end_y);
-
-            // println!("{:?}", coordinates);
-            // println!("{:?} {:?} {:?} {:?}", start_x, start_y, end_x, end_y);
 
             let mut sorted_x: [usize; 2] = [start_x, end_x];
             let mut sorted_y: [usize; 2] = [start_y, end_y];
             sorted_x.sort();
             sorted_y.sort();
 
-            for x in sorted_x[0]..sorted_x[1]+1 {
-                for y in sorted_y[0]..sorted_y[1]+1 {
-                    // println!("{:?}, {:?}", x, y);
-                    occupied.insert((x,y));
+            for x in sorted_x[0]..sorted_x[1] + 1 {
+                for y in sorted_y[0]..sorted_y[1] + 1 {
+                    occupied.insert((x, y));
                 }
             }
 
@@ -43,15 +70,12 @@ pub fn y22d14(input: &str, part: u32) -> u32 {
         }
     }
 
-    // println!("{:?}", occupied);
-
-    let floor = if part ==1 {
+    let floor = if part == 1 {
         lowest.pop().unwrap()
     } else {
         lowest.pop().unwrap() + 2
     };
 
-    // println!("floor is: {:?}", floor);
     let mut stop = false;
     let mut total = 0;
     while !stop {
@@ -62,53 +86,36 @@ pub fn y22d14(input: &str, part: u32) -> u32 {
 
             if y > floor {
                 occupied.insert((x, floor));
-                occupied.insert((x-1, floor));
-                occupied.insert((x+1, floor));
+                occupied.insert((x - 1, floor));
+                occupied.insert((x + 1, floor));
                 total -= 1;
                 break;
             }
 
-            if part == 1 {
-                let below: Vec<_> = occupied.iter().filter(|o| {
-                    let (ox, oy) = o;
-                    // println!("ox: {:?}, oy: {:?}, x: {:?}, y: {:?}", ox, oy, x, y);
-                    ox == &x && oy > &y
-                }).collect();
-
-                if below.is_empty() {
-                    // sand falls forever; we're done
-                    // println!("sand is {:?} and it will fall forever", sand);
-                    stop = true;
-                    break;
-                }
+            // NB clippy suggests using find instead of filter().next() but
+            // HashSet doesn't implement find...
+            if part == 1
+                && occupied
+                    .iter()
+                    .filter(|o| {
+                        let (ox, oy) = o;
+                        ox == &x && oy > &y
+                    })
+                    .next()
+                    .is_none()
+            {
+                // sand falls forever; we're done
+                stop = true;
+                break;
             }
 
-
-            // println!("{:?}", occupied);
-            // println!("{:?}", below);
-
-            // if below.is_empty() {
-            //     if part == 1 {
-            //         // sand falls forever; we're done
-            //         // println!("sand is {:?} and it will fall forever", sand);
-            //         stop = true;
-            //         break;
-            //     } else {
-            //         // add the floor and keep going
-            //         occupied.insert((x, floor));
-            //         occupied.insert((x-1, floor));
-            //         occupied.insert((x+1, floor));
-            //     }
-            // }
-
-            if !occupied.contains(&(x, y+1)) {
-                sand = (x,y+1);
-            } else if !occupied.contains(&(x-1, y+1)) {
-                sand = (x-1,y+1);
-            } else if !occupied.contains(&(x+1,y+1)) {
-                sand = (x+1, y+1);
+            if !occupied.contains(&(x, y + 1)) {
+                sand = (x, y + 1);
+            } else if !occupied.contains(&(x - 1, y + 1)) {
+                sand = (x - 1, y + 1);
+            } else if !occupied.contains(&(x + 1, y + 1)) {
+                sand = (x + 1, y + 1);
             } else {
-                // println!("sand stopped at: {:?}", sand);
                 break;
             }
         }
@@ -122,11 +129,10 @@ pub fn y22d14(input: &str, part: u32) -> u32 {
         total += 1
     }
 
-
-    if part == 1{
-        total -1
+    if part == 1 {
+        total - 1
     } else {
-        total +1
+        total + 1
     }
 }
 
@@ -140,7 +146,7 @@ mod tests {
         let input = concat!(
             "498,4 -> 498,6 -> 496,6\n",
             "503,4 -> 502,4 -> 502,9 -> 494,9\n"
-            );
+        );
 
         assert_eq!(y22d14(input, 1), 24);
         assert_eq!(y22d14(input, 2), 93);
@@ -151,6 +157,6 @@ mod tests {
         let contents = fs::read_to_string("input/2022/day14.txt").unwrap();
 
         assert_eq!(y22d14(&contents, 1), 698);
-        assert_eq!(y22d14(&contents, 1), 28594);
+        assert_eq!(y22d14(&contents, 2), 28594);
     }
 }
