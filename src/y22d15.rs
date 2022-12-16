5d494906f7b3db0f154dd8c1630b16b5f7791ddf
Mario Finelli <mario@finel.li>
2022-12-16 09:53:59 +0100

    Save work in progress for day fifteen
    Building the sensor ranges works for small inputs but takes too long on
    the actual input.

--- src/y22d15.rs
+++ src/y22d15.rs
@@ -1,24 +1,89 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use regex::Regex;
 
 pub fn y22d15(input: &str, row: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
-    let mut sensors: HashMap<(i32, i32), Vec<(i32, i32)>> = HashMap::new();
+    let mut sensors: HashMap<(i32, i32), HashSet<(i32, i32)>> = HashMap::new();
     let r = Regex::new(r"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$").unwrap();
 
     for line in lines {
         let captures = r.captures(line).unwrap();
-        // println!("{:?}", captures);
+        println!("{:?}", captures);
         let sx: i32 = captures[1].parse().unwrap();
         let sy: i32 = captures[2].parse().unwrap();
         let bx: i32 = captures[3].parse().unwrap();
         let by: i32 = captures[4].parse().unwrap();
 
-        let mut range: Vec<(i32, i32)> = Vec::new();
+        let mut range: HashSet<(i32, i32)> = HashSet::new();
+        range.insert((sx,sy));
+
+        let mut i = 1;
+        loop {
+            if range.contains(&(bx,by)) {
+                break;
+            }
+
+            for (j,x) in (sx..sx+i).enumerate() {
+                // println!("need to write coord {},?", x);
+                for y in sy..sy+i-j as i32 {
+                    // println!("write coord {}, {}", x, y);
+                    range.insert((x, y));
+                }
+
+                for y in (sy-i+j as i32..sy).rev() {
+                    // println!("write coord {}, {}", x, y);
+                    range.insert((x, y+1));
+                }
+            }
+
+            for (j,x) in (sx-i..sx).rev().enumerate() {
+                // println!("write coord {}, ?", x);
+                for y in sy..sy+i-j as i32 {
+                    // println!("write coord {}, {}", x-1, y);
+                    range.insert((x+1, y));
+                }
+
+                for y in (sy-i+j as i32..sy).rev() {
+                    range.insert((x+1, y+1));
+                }
+            }
+
+
+            // if i == 1 {
+            //     range.insert((sx,sy+1));
+            //     range.insert((sx,sy-1));
+            //     range.insert((sx+1,sy));
+            //     range.insert((sx-1,sy));
+            // } else {
+            //     range.insert((sx, sy+i));
+            //     for (k,j) in (sy..sy+i+1).rev().enumerate() {
+            //         // println!("{},{}", k, j);
+            //         // let j = i - j;
+            //         range.insert((sx+j, sy+i-j));
+            //     }
+            // }
+
+            // for j in 0..i {
+            //     range.insert((
+            // }
+
+            i += 1;
+
+
+
+
+            // if i == 5 {
+            //     break;
+            // }
+        }
+
 
         sensors.insert((sx, sy), range);
 
 
+        println!("{:?}", sensors);
+        // break;
 
 
     }
