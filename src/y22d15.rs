d1e3ace533a71a6e53595db33241bd029732225f
Mario Finelli <mario@finel.li>
2022-12-17 12:59:02 -0500

    Do initial cleanup for day fifteen solution

--- src/y22d15.rs
+++ src/y22d15.rs
@@ -1,7 +1,33 @@
-use regex::Regex;
-use std::collections::HashMap;
-use std::collections::HashSet;
+/* Copyright 2022 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+//! Advent of Code 2022 Day 15: <https://adventofcode.com/2022/day/15>
+//!
+//! TODO
+
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+/// The solution for part one of the day fifteen challenge.
+///
+/// TODO
+///
+/// # Example
+/// ```rust
+/// # use aoc::y22d15::y22d15p1;
+/// ```
 pub fn y22d15p1(input: &str, row: i32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
     let mut rows: HashMap<i32, Vec<(i32, i32)>> = HashMap::new();
@@ -10,17 +36,17 @@ pub fn y22d15p1(input: &str, row: i32) -> u32 {
 
     for line in lines {
         let captures = r.captures(line).unwrap();
-        // println!("{:?}", captures);
         let sx: i32 = captures[1].parse().unwrap();
         let sy: i32 = captures[2].parse().unwrap();
         let bx: i32 = captures[3].parse().unwrap();
         let by: i32 = captures[4].parse().unwrap();
 
+        // we'll need to subtract all of the beacons that might be on the row
+        // that we're interested in from the final total as they obviously
+        // don't count as "positions that can't contain a beacon"
         if by == row {
             beacons.insert((bx, by));
         }
-
-        // let mut range: HashSet<(i32, i32)> = HashSet::new();
 
         let md = (sx - bx).abs() + (sy - by).abs();
 
@@ -30,10 +56,6 @@ pub fn y22d15p1(input: &str, row: i32) -> u32 {
             let up_y = sy + i as i32;
             let down_y = sy - i as i32;
 
-            // println!("run line from {} to {} on {}", start_x, end_x, up_y);
-            // println!("run line from {} to {} on {}", start_x, end_x, down_y);
-            // for x in start_x..end_x +1 {
-            // range.insert((x, up_y));
             if up_y == row {
                 let mut up_ranges = rows.entry(up_y).or_insert(Vec::new());
                 up_ranges.push((start_x, end_x));
@@ -42,61 +64,12 @@ pub fn y22d15p1(input: &str, row: i32) -> u32 {
             if i != 0 && down_y == row {
                 let mut down_ranges = rows.entry(down_y).or_insert(Vec::new());
                 down_ranges.push((start_x, end_x));
-                // range.insert((x, down_y));
             }
-            // }
         }
-        // range.insert((sx,sy));
-
-        // let mut i = 1;
-        // loop {
-        //     if range.contains(&(bx,by)) {
-        //         break;
-        //     }
-
-        //     println!("i is {}", i);
-
-        //     for (j,x) in (sx..sx+i).enumerate() {
-        //         println!("need to write coord {},?", x);
-        //         for y in sy..sy+i-j as i32 {
-        //             println!("write coord {}, {}", x, y);
-        //             range.insert((x, y));
-        //         }
-
-        //         println!("need to write coord {},? (p2)", x);
-        //         for y in (sy-i+j as i32..sy).rev() {
-        //             println!("write coord {}, {}", x, y+1);
-        //             range.insert((x, y+1));
-        //         }
-        //     }
-
-        //     for (j,x) in (sx-i..sx).rev().enumerate() {
-        //         println!("need to write coord {},? (down)", x+1);
-        //         for y in sy..sy+i-j as i32 {
-        //             println!("write coord {}, {}", x+1, y);
-        //             range.insert((x+1, y));
-        //         }
-
-        //         println!("need to write coord {},? (down p2)", x+1);
-        //         for y in (sy-i+j as i32..sy).rev() {
-        //             println!("write coord {}, {}", x+1, y+1);
-        //             range.insert((x+1, y+1));
-        //         }
-        //     }
-
-        //     i += 1;
-        // }
-
-        // sensors.insert((sx, sy), range);
-
-        // println!("{:?}", sensors);
-        // break;
     }
 
-    // println!("{:?}", rows);
     let row = rows.get(&row).unwrap();
     let mut matches: HashSet<i32> = HashSet::new();
-    // println!("{:?}", row);
     for r in row {
         let (start, end) = r;
         for x in *start..*end + 1 {
@@ -104,24 +77,17 @@ pub fn y22d15p1(input: &str, row: i32) -> u32 {
         }
     }
 
-    // println!("{:?}", matches);
-
-    // let mut matches = HashSet::new();
-    // for (sensor, range) in sensors {
-    //     for r in range {
-    //         let (x,y) = r;
-    //         if y == row {
-    //             matches.insert(r);
-    //         }
-    //     }
-    // }
-
-    // subtract one to account for the actual beacon
-    // println!("{:?}", beacons);
     matches.len() as u32 - beacons.len() as u32
-    // 0
 }
 
+/// The solution for part two of the day fifteen challenge.
+///
+/// TODO
+///
+/// # Example
+/// ```rust
+/// # use aoc::y22d15::y22d15p2;
+/// ```
 pub fn y22d15p2(input: &str, max: i32) -> u64 {
     let lines: Vec<_> = input.lines().collect();
     let r = Regex::new(r"^Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)$").unwrap();
@@ -130,74 +96,57 @@ pub fn y22d15p2(input: &str, max: i32) -> u64 {
 
     for line in lines {
         let captures = r.captures(line).unwrap();
-        // println!("{:?}", captures);
         let sx: i32 = captures[1].parse().unwrap();
         let sy: i32 = captures[2].parse().unwrap();
         let bx: i32 = captures[3].parse().unwrap();
         let by: i32 = captures[4].parse().unwrap();
 
-        let md = (sx-bx).abs() + (sy-by).abs();
-        sensors.insert((sx,sy), md);
+        let md = (sx - bx).abs() + (sy - by).abs();
+        sensors.insert((sx, sy), md);
     }
 
-    // println!("{:?}", sensors);
-
-    for ((sx,sy), md) in &sensors {
-        // if sx != &12 && sy != &14 {
-        //     continue;
-        // }
-
+    for ((sx, sy), md) in &sensors {
+        // use manhattan distance plus two to account for being one _larger_
+        // than the border (plus one to make it inclusive, plus one for the
+        // larger size)
         for i in 0..md + 2 {
-            let start_x = sx - md +i as i32;
-            let end_x = sx +md -i as i32;
+            let start_x = sx - md + i as i32;
+            let end_x = sx + md - i as i32;
             let up_y = sy + i as i32;
             let down_y = sy - i as i32;
 
-            // println!("sensor {},{} has border {},{}", sx, sy, start_x, up_y);
-            // println!("sensor {},{} has border {},{}", sx, sy, start_x, down_y);
-            // println!("sensor {},{} has border {},{}", sx, sy, end_x, up_y);
-            // println!("sensor {},{} has border {},{}", sx, sy, end_x, down_y);
+            let checks: [(i32, i32); 4] = [
+                (start_x - 1, up_y),
+                (end_x + 1, up_y),
+                (start_x - 1, down_y),
+                (end_x + 1, down_y),
+            ];
 
-            let checks: [(i32, i32); 4] = [(start_x-1, up_y), (end_x+1, up_y), (start_x -1, down_y), (end_x +1,down_y)];
-
-            for (x,y) in checks {
+            for (x, y) in checks {
                 if x < 0 || x > max || y < 0 || y > max {
                     continue;
                 }
 
-                // println!("sensor {},{} outside border: {},{}", sx, sy, x, y);
-
-                // if x == 14 && y == 11 {
-                //     println!("found 14,11: {:?},{:?} {:?}", sx,sy,md);
-                // }
-
                 let mut in_range = false;
-                for ((ox,oy), omd) in &sensors {
+                for ((ox, oy), omd) in &sensors {
                     if ox == sx && oy == sy {
                         continue;
                     }
 
-                    if (ox-x).abs() + (y-oy).abs() < *omd {
-                        // println!("{:?},{:?} is in range of {:?},{:?}", x,y,ox,oy);
+                    if (ox - x).abs() + (y - oy).abs() < *omd {
                         in_range = true;
                         break;
                     }
                 }
 
                 if !in_range {
-                    // println!("{},{} is not in range of any sensor", x,y);
                     return (x as i64 * 4000000 + y as i64).try_into().unwrap();
-                    // break;
                 }
             }
         }
-
     }
 
-
-
     // we shouldn't get here...
-
     0
 }
 
@@ -226,6 +175,8 @@ mod tests {
         );
 
         assert_eq!(y22d15p1(input, 10), 26);
+        // TODO: there's an ordering problem here somewhere... this test
+        // occasionally fails
         assert_eq!(y22d15p2(input, 20), 56000011);
     }
 
