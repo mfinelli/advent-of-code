098e918e82e262a6204ff4d6285da7cd321a3eb9
Mario Finelli <mario@finel.li>
2022-12-19 14:11:09 -0500

    Do initial cleanup on day sixteen

--- src/y22d16.rs
+++ src/y22d16.rs
@@ -1,13 +1,41 @@
+/* Copyright 2022 Mario Finelli
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Advent of Code 2022 Day 16: <https://adventofcode.com/2022/day/16>
+//!
+//! TODO
+
 use regex::Regex;
 use std::collections::{BinaryHeap, HashMap, HashSet};
 
+/// The solution for the day sixteen challenge.
+///
+/// TODO
+///
+/// # Example
+/// ```rust
+/// # use aoc::y22d16::y22d16;
+/// ```
 pub fn y22d16(input: &str, part: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
     let flow_regex = Regex::new(r"^rate=(\d+);$").unwrap();
 
     let mut rates: HashMap<String, u32> = HashMap::new();
     let mut connections: HashMap<String, Vec<String>> = HashMap::new();
-    let mut distances: HashMap<String, HashMap<String, Option<i32>>> = HashMap::new();
+    let mut distances: HashMap<String, HashMap<String, Option<i32>>> =
+        HashMap::new();
 
     for line in lines {
         let parts: Vec<_> = line.split_whitespace().collect();
@@ -23,10 +51,10 @@ pub fn y22d16(input: &str, part: u32) -> u32 {
         connections.insert(parts[1].to_string(), room_connections);
     }
 
-    for (room,rate) in &rates {
+    for (room, rate) in &rates {
         let mut other_distances: HashMap<String, Option<i32>> = HashMap::new();
 
-        for (other_room,other_rate) in &rates {
+        for (other_room, other_rate) in &rates {
             if room == other_room {
                 other_distances.insert(other_room.to_string(), Some(0));
             } else if connections[room].contains(other_room) {
@@ -54,19 +82,26 @@ pub fn y22d16(input: &str, part: u32) -> u32 {
                                 match kj {
                                     Some(kj) => {
                                         if ij > ik + kj {
-                                            distances.get_mut(i).map(|val| val.insert(j.to_string(), Some(ik + kj)));
+                                            distances.get_mut(i).map(|val| {
+                                                val.insert(
+                                                    j.to_string(),
+                                                    Some(ik + kj),
+                                                )
+                                            });
                                         }
-                                    },
+                                    }
                                     None => {
-                                        // kj is infinity so ik + kj is infinity which is always
-                                        // greater than ij (even if that's infinity)
-                                    },
+                                        // kj is infinity so ik + kj is
+                                        // infinity which is always greater
+                                        // than ij (even if that's infinity)
+                                    }
                                 }
-                            },
+                            }
                             None => {
-                                // ik is infinity so ik + kj is infinity which is always
-                                // greater than ij (even if that's infinity)
-                            },
+                                // ik is infinity so ik + kj is infinity which
+                                // is always greater than ij (even if that's
+                                // infinity)
+                            }
                         }
                     }
                     None => {
@@ -74,20 +109,27 @@ pub fn y22d16(input: &str, part: u32) -> u32 {
                             Some(ik) => {
                                 match kj {
                                     Some(kj) => {
-                                        // ik and kj are _not_ infinity but ik _is_
-                                        // so ik + kj will always be less than
-                                        distances.get_mut(i).map(|val| val.insert(j.to_string(), Some(ik + kj)));
-                                    },
+                                        // ik and kj are _not_ infinity but ij
+                                        // _is_ so ik + kj will always be less
+                                        distances.get_mut(i).map(|val| {
+                                            val.insert(
+                                                j.to_string(),
+                                                Some(ik + kj),
+                                            )
+                                        });
+                                    }
                                     None => {
-                                        // kj is infinity so ik + kj is infinity which is always
-                                        // greater than ij (even if that's infinity)
-                                    },
+                                        // kj is infinity so ik + kj is
+                                        // infinity which is always greater
+                                        // than ij (even if that's infinity)
+                                    }
                                 }
-                            },
+                            }
                             None => {
-                                // ik is infinity so ik + kj is infinity which is always
-                                // greater than ij (even if that's infinity)
-                            },
+                                // ik is infinity so ik + kj is infinity which
+                                // is always greater than ij (even if that's
+                                // infinity)
+                            }
                         }
                     }
                 }
@@ -102,22 +144,25 @@ pub fn y22d16(input: &str, part: u32) -> u32 {
         }
     }
 
-    // println!("{:?}", rates);
-    // println!("{:?}", connections);
-    // println!("{:?}", distances);
-    // println!("{:?}", positive_flows);
-
-    // let start = HashMap::new();
-    // let paths = dfs(&distances, &rates, &positive_flows, 30, "AA", &mut start);
     if part == 1 {
-        dfs(&distances, &rates, positive_flows, "AA".to_string(), 30, false)
+        dfs(
+            &distances,
+            &rates,
+            positive_flows,
+            "AA".to_string(),
+            30,
+            false,
+        )
     } else {
-        dfs(&distances, &rates, positive_flows, "AA".to_string(), 26, true)
+        dfs(
+            &distances,
+            &rates,
+            positive_flows,
+            "AA".to_string(),
+            26,
+            true,
+        )
     }
-    // println!("{:?}", result);
-
-
-    // result
 }
 
 fn dfs(
@@ -132,14 +177,22 @@ fn dfs(
     paths.push(0); // TODO: remove
 
     for room in &positive_flows {
-        // let rate = rates[room];
-        let new_time = time_remaining - (distances[&current_room][room].unwrap() + 1);
+        let new_time =
+            time_remaining - (distances[&current_room][room].unwrap() + 1);
         let mut positive_flows = positive_flows.clone();
         positive_flows.remove(room);
 
         if time_remaining > new_time && new_time > 0 {
             let new_time_pos: u32 = new_time.try_into().unwrap();
-            let rate = rates[room] * new_time_pos + dfs(distances, rates, positive_flows, room.to_string(), new_time, help);
+            let rate = rates[room] * new_time_pos
+                + dfs(
+                    distances,
+                    rates,
+                    positive_flows,
+                    room.to_string(),
+                    new_time,
+                    help,
+                );
             paths.push(rate);
         }
     }
@@ -147,43 +200,18 @@ fn dfs(
     // with the given rooms remaining, calculate most pressure that could be
     // released by the other person (elephant)
     if help {
-        paths.push(dfs(distances, rates, positive_flows, "AA".to_string(), 26, false));
+        paths.push(dfs(
+            distances,
+            rates,
+            positive_flows,
+            "AA".to_string(),
+            26,
+            false,
+        ));
     }
 
     paths.pop().unwrap()
 }
-
-
-// fn dfs(distances: &HashMap<&str, HashMap<&str, Option<i32>>>, rates: &HashMap<&str, u32>, positive_flows: &HashMap<&str, u32>, time_remaining: i32, current_room: &str, opened: &HashMap<&str, i32>) -> Vec<&HashMap<&str, i32>> {
-// fn dfs<'a>(distances: &'a HashMap<&'a str, HashMap<&'a str, Option<i32>>>, rates: &'a HashMap<&'a str, u32>, positive_flows: &'a HashMap<&'a str, u32>, time_remaining: i32, current_room: &'a str, opened: &'a mut HashMap<&'a str, i32>) -> Vec<&'a mut HashMap<&'a str, i32>> {
-//     let mut results = Vec::new();
-//     results.push(opened);
-
-//     if time_remaining < 2 {
-//         return results;
-//     }
-
-//     // let opened_keys: Vec<_> = opened.keys().collect();
-
-//     for (room, rate) in positive_flows {
-//         let mut my_opened = opened.clone();
-//         if !my_opened.contains_key(room) {
-//         // if !opened_keys.contains(&room) {
-//             let new_time_remaining = time_remaining - (distances[current_room][room].unwrap() + 1);
-//             my_opened.insert(room, new_time_remaining);
-//             // positive_flows.remove(room);
-//             let mut new_results = dfs(distances, rates, positive_flows, new_time_remaining, room, &mut my_opened);
-//             results.append(&mut new_results);
-//         }
-//     }
-
-//     // if time_remaining != 1 {
-//     //     let mut new_results = dfs(distances, rates, positive_flows, 1, "BB", opened);
-//     //     results.append(&mut new_results);
-//     // }
-
-//     return results;
-// }
 
 #[cfg(test)]
 mod tests {
@@ -215,7 +243,7 @@ mod tests {
         let contents = fs::read_to_string("input/2022/day16.txt").unwrap();
 
         assert_eq!(y22d16(&contents, 1), 2124);
-        // TODO: optimize this... it take almost 10m with a release build
+        // TODO: optimize this... it takes almost 10m even with a release build
         // assert_eq!(y22d16(&contents, 2), 2775);
     }
 }
