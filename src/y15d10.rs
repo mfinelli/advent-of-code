a9f42d132515620dcdf527b5327c123ce8b33e06
Mario Finelli <mario@finel.li>
2023-11-27 15:58:22 +0200

    Add write-up for day 10

--- src/y15d10.rs
+++ src/y15d10.rs
@@ -15,11 +15,16 @@
 
 //! Advent of Code 2015 Day 10: <https://adventofcode.com/2015/day/10>
 //!
-//! TODO
+//! It took me a little while to wrap my head around what look-and-say is but
+//! once I figured it out this was relatively easy to implement.
 
 /// The solution for the day nine challenge.
 ///
-/// TODO
+/// We take the input as a string and the number of times to perform the
+/// look-and-say as arguments and then return the length of the resulting
+/// string as requested by the prompt. We start by converting the input string
+/// into a vector of integers and then run the look-and-say function on the
+/// result over and over until we've run it the requested number of times.
 ///
 /// # Example
 /// ```rust
@@ -42,20 +47,25 @@ pub fn y15d10(input: &str, times: u32) -> u32 {
     nums.len().try_into().unwrap()
 }
 
-/// TODO
+/// This function implements the look-and-say algorithm. It's relatively
+/// straightforward: we mark the first digit as the "previous" and then start
+/// counting from the second digit (if there is one). At each digit we check
+/// to see if the digit is the same as the previous one. If it is then we
+/// increment the current counter. If it's not then we add the current count
+/// and "previous" into the final result, and then set the "previous" to the
+/// new current and then reset the counter. Finally, push the final count and
+/// digit onto the vector and we're done.
 fn lookandsay(input: Vec<u32>) -> Vec<u32> {
     let mut result: Vec<u32> = Vec::new();
 
     let mut previous = input[0];
     let mut count = 1;
 
-    for i in 1..input.len() {
-        let digit = input[i];
-
-        if digit != previous {
+    for digit in input.iter().skip(1) {
+        if *digit != previous {
             result.push(count);
             result.push(previous);
-            previous = digit;
+            previous = *digit;
             count = 1;
         } else {
             count += 1;
