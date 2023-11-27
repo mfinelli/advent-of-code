92752d35c9201554080ebac4c0c560b4262e15ef
Mario Finelli <mario@finel.li>
2023-11-27 11:32:52 +0200

    Add write-up for day nine

--- src/y15d09.rs
+++ src/y15d09.rs
@@ -15,7 +15,10 @@
 
 //! Advent of Code 2015 Day 9: <https://adventofcode.com/2015/day/9>
 //!
-//! TODO
+//! This challenge turned out to be pretty easy. The list of inputs is small
+//! so we don't need to worry about building any graphs and then trying to
+//! find the shortest path or anything. We can just compute all of the possible
+//! trips and their distances and then get the shortest/longest.
 
 use itertools::Itertools;
 use std::cmp::Reverse;
@@ -23,14 +26,25 @@ use std::collections::{BinaryHeap, HashMap};
 
 /// The solution for the day nine challenge.
 ///
-/// TODO
+/// We take the input as a string and the problem part as an integer as usual.
+/// We start by iterating over the lines and parsing the city names and the
+/// distances. We maintain a [`std::collections::HashMap`] of the city names
+/// with their values equal to another HashMap that contains the other cities
+/// and their distances. This way we can lookup the distance from one city to
+/// any other city. Then we get all of the permutations of the cities which
+/// gives us all of the possible trips that we could take and then look at them
+/// by two to give us each leg of the trip. Just get the distance for the trip
+/// and then add it to the [`std::collections::BinaryHeap`]s that we've been
+/// maintaining of the trip distances. If we're on part one then get the value
+/// from the min-heap, otherwise the value from the max-heap.
 ///
 /// # Example
 /// ```rust
 /// # use aoc::y15d09::y15d09;
 /// // probably read this from the input file...
-/// let input = "";
-/// assert_eq!(y15d09(input), 0);
+/// let input = "A to B = 10\nB to C = 15\nA to C = 25\n";
+/// assert_eq!(y15d09(input, 1), 25);
+/// assert_eq!(y15d09(input, 2), 40);
 /// ```
 pub fn y15d09(input: &str, part: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
