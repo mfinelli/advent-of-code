020b9db5ca7f51aaa4aa67af36ac6eca3a2bc4ff
Mario Finelli <mario@finel.li>
2023-11-29 10:18:20 +0200

    Add write-up and tests for day fifteen

--- src/y15d15.rs
+++ src/y15d15.rs
@@ -15,7 +15,26 @@
 
 //! Advent of Code 2015 Day 15: <https://adventofcode.com/2015/day/15>
 //!
-//! TODO
+//! Today's problem was a little challenging. Looking for inspiration for how
+//! to solve it I looked at some of the solutions on the advent of code
+//! subreddit but found that almost all of the solutions brute-forced/hardcoded
+//! the number of ingredients to four. This was unacceptable to me because I
+//! wanted a generic solution that would work for any number of ingredients
+//! (also because I wanted to be able to use the same functions to test my
+//! solution against the examples which only had two ingredients). I originally
+//! brute-forced it in my own way by generating a huge vector with all of the
+//! possible ingredients and the possible teaspoons from 0-100 and then
+//! generated all of the combinations (take length of ingredients) filtering
+//! out any combination that didn't add up to the correct number of teaspoons
+//! and/or didn't use all four (or length of ingredients) of the ingredients.
+//! This worked and provided the correct answer, but it was painfully slow. On
+//! my machine each part took about five minutes. To improve this I knew that
+//! I needed to reduce the overall number of possible ingredient combinations.
+//! I did this by instead computing the cartesian product of a vector for each
+//! ingredient in all of its possible quantities. This greatly reduced the
+//! overall number of combinations that needed to be checked/excluded. While
+//! it still doesn't run extremely fast each part now runs in about 30 seconds
+//! which for a generic solution is acceptable to me.
 
 use itertools::Itertools;
 use std::collections::BinaryHeap;
@@ -48,7 +67,19 @@ struct Mix<'a> {
 
 /// The solution for the day fifteen challenge.
 ///
-/// TODO
+/// As usual we take the input as a string and a variable to determine which
+/// part we're computing (in part `2` we need to limit the options to cookies
+/// that result in 500 calories). We start by processing the input and building
+/// `Ingredients` to keep track of the various qualities of the ingredients.
+/// We then create a vector of vectors, one for each ingredient where the
+/// sub-vector are all of the possible teaspoons of the ingredient to use to
+/// make the cookie (0 to 100). We then take the cartesian product of those
+/// vectors to get all of the possible recipes (both valid and invalid) and
+/// then filter the result to only the valid options (i.e., that use 100 total
+/// teaspoons, and in part `2` are 500 calories). With the list of valid
+/// recipes we compute the score of each one and add it to our
+/// [`std::collections::BinaryHeap`]. To get the highest possible score we just
+/// pop the heap.
 ///
 /// # Example
 /// ```rust
@@ -89,10 +120,7 @@ pub fn y15d15(input: &str, part: u32) -> i32 {
     for ingredient in &ingredients {
         let mut totals = Vec::new();
         for amount in 0..TOTAL + 1 {
-            totals.push(Mix {
-                amount,
-                ingredient,
-            });
+            totals.push(Mix { amount, ingredient });
         }
         mixes.push(totals);
     }
@@ -110,7 +138,9 @@ pub fn y15d15(input: &str, part: u32) -> i32 {
     scores.pop().unwrap()
 }
 
-/// TODO
+/// This function determines whether the provided vector of ingredients and
+/// their quantities are valid: the quantities should add up to 100 and if
+/// we're in part `2` then the sum of the calories should add up to 500.
 fn valid_possibility(possibility: &[Mix], part: u32) -> bool {
     let sum: i32 = possibility.iter().map(|p| p.amount).sum();
     if sum != TOTAL {
@@ -130,7 +160,12 @@ fn valid_possibility(possibility: &[Mix], part: u32) -> bool {
     true
 }
 
-/// TODO
+/// This function computes the score of a combination of ingredients and their
+/// quantities. For each of the qualities of an ingredient it's multiplied by
+/// the quantity and then summed with the result of that quality for the other
+/// ingredients in the combination. If any quality results in a negative
+/// number then it turns to zero and the entire score is zero (as described by
+/// the prompt).
 fn compute_score(possibility: &[Mix]) -> i32 {
     let capacity: i32 = possibility
         .iter()
@@ -173,10 +208,136 @@ mod tests {
     use std::fs;
 
     #[test]
-    fn test_compute_score() {}
+    fn test_compute_score() {
+        let mut input = vec![Mix {
+            ingredient: &Ingredient {
+                capacity: 3,
+                durability: 4,
+                flavor: 5,
+                texture: 6,
+                calories: 7,
+            },
+            amount: 2,
+        }];
+
+        assert_eq!(compute_score(&input), 5760);
+
+        input = vec![Mix {
+            ingredient: &Ingredient {
+                capacity: 2,
+                durability: 4,
+                flavor: 2,
+                texture: -6,
+                calories: 7,
+            },
+            amount: 3,
+        }];
+
+        assert_eq!(compute_score(&input), 0);
+    }
 
     #[test]
-    fn test_valid_possibility() {}
+    fn test_valid_possibility() {
+        let mut input = vec![
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 1,
+                },
+                amount: 1,
+            },
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 1,
+                },
+                amount: 1,
+            },
+        ];
+
+        assert!(!valid_possibility(&input, 1));
+
+        input = vec![
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 1,
+                },
+                amount: 50,
+            },
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 1,
+                },
+                amount: 50,
+            },
+        ];
+
+        assert!(valid_possibility(&input, 1));
+
+        input = vec![
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 4,
+                },
+                amount: 50,
+            },
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 5,
+                },
+                amount: 50,
+            },
+        ];
+
+        assert!(!valid_possibility(&input, 2));
+
+        input = vec![
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 1,
+                },
+                amount: 0,
+            },
+            Mix {
+                ingredient: &Ingredient {
+                    capacity: 1,
+                    durability: 1,
+                    flavor: 1,
+                    texture: 1,
+                    calories: 5,
+                },
+                amount: 100,
+            },
+        ];
+
+        assert!(valid_possibility(&input, 2));
+    }
 
     #[test]
     fn it_works() {
