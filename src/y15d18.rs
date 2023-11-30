e22a4c7560b1d50b8afea4fb8051d9f19c24f111
Mario Finelli <mario@finel.li>
2023-11-30 22:38:40 +0100

    Fix style and clippy errors

--- src/y15d18.rs
+++ src/y15d18.rs
@@ -29,100 +29,85 @@ use std::collections::HashMap;
 /// // probably read this from the input file...
 /// let input = ".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..";
 /// assert_eq!(y15d18(input, 1, 1), 11);
+/// assert_eq!(y15d18(input, 3, 2), 18);
 /// ```
 pub fn y15d18(input: &str, steps: u32, part: u32) -> u32 {
     let lines: Vec<_> = input.lines().collect();
-    let size = lines.len();
+    let size: i32 = lines.len().try_into().unwrap();
     let mut grid = HashMap::new();
 
     for (y, line) in lines.iter().enumerate() {
         for (x, light) in line.chars().enumerate() {
-            grid.insert((x as i32,y as i32), light == '#');
+            grid.insert((x as i32, y as i32), light == '#');
         }
     }
 
     if part == 2 {
         // in part two the corners are always on
         grid.insert((0, 0), true);
-        grid.insert((size as i32 -1, 0), true);
-        grid.insert((0, size as i32 -1), true);
-        grid.insert((size as i32 -1, size as i32 -1), true);
+        grid.insert((size - 1, 0), true);
+        grid.insert((0, size - 1), true);
+        grid.insert((size - 1, size - 1), true);
     }
 
     for _ in 0..steps {
-        print_grid(&grid, size);
         let mut newgrid = HashMap::new();
 
-        for ((x,y), light) in &grid {
-            if part == 2 {
-                if (*x == 0 && *y == 0) || (*x == size as i32 -1 && *y == 0) || (*x == 0 && *y == size as i32 -1) || (*x == size as i32 -1 && *y == size as i32 -1){
-                    continue;
-                }
+        for ((x, y), light) in &grid {
+            if part == 2
+                && ((*x == size - 1 || *x == 0) && *y == 0
+                    || (*x == 0 && *y == -1)
+                    || (*x == size - 1 && *y == size - 1))
+            {
+                continue;
             }
 
-            let neighbors = vec![(*x-1, *y-1), (*x, *y-1), (*x+1,*y-1),(*x+1,*y),(*x+1,*y+1),(*x,*y+1),(*x-1,*y+1),(*x-1,*y)];
+            let neighbors = vec![
+                (*x - 1, *y - 1),
+                (*x, *y - 1),
+                (*x + 1, *y - 1),
+                (*x + 1, *y),
+                (*x + 1, *y + 1),
+                (*x, *y + 1),
+                (*x - 1, *y + 1),
+                (*x - 1, *y),
+            ];
             let mut on = 0;
 
-            // if *x == 0 && *y == 0 {
             for neighbor in neighbors {
-                if let Some(a) = grid.get(&neighbor) {
-                    if *a {
-                    // println!("{:?}", a);
-                    on += 1;
+                if let Some(switch) = grid.get(&neighbor) {
+                    if *switch {
+                        on += 1;
                     }
                 }
             }
-            // println!("{:?}", on);
-            // }
-
-            // println!("step: {} - ({}, {}): {:?}", s, x, y, on);
 
             if *light {
                 if on == 2 || on == 3 {
-                    newgrid.insert((*x,*y), true);
+                    newgrid.insert((*x, *y), true);
                 } else {
-                    newgrid.insert((*x,*y), false);
+                    newgrid.insert((*x, *y), false);
                 }
+            } else if on == 3 {
+                newgrid.insert((*x, *y), true);
             } else {
-                if on == 3 {
-                    newgrid.insert((*x,*y), true);
-                } else {
-                    newgrid.insert((*x,*y), false);
-                }
+                newgrid.insert((*x, *y), false);
             }
         }
 
         if part == 2 {
             // in part two the corners are always on
             newgrid.insert((0, 0), true);
-            newgrid.insert((size as i32 -1, 0), true);
-            newgrid.insert((0, size as i32 -1), true);
-            newgrid.insert((size as i32 -1, size as i32 -1), true);
+            newgrid.insert((size - 1, 0), true);
+            newgrid.insert((0, size - 1), true);
+            newgrid.insert((size - 1, size - 1), true);
         }
 
         grid = newgrid;
     }
 
-    grid.retain(|_, x| *x); // retiain lights that are on
+    grid.retain(|_, x| *x); // retain lights that are on
     grid.len().try_into().unwrap()
-    // 0
-}
-
-fn print_grid(grid: &HashMap<(i32,i32), bool>, size: usize) {
-    // println!("{:?}", grid);
-    let mut s = "".to_string();
-    for y in 0..size {
-        for x in 0..size {
-            if *grid.get(&(x as i32, y as i32)).unwrap() {
-                s = format!("{}{}", s, '#');
-            } else {
-                s = format!("{}{}", s, '.');
-            }
-        }
-        s = format!("{}{}", s, "\n");
-    }
-
-    println!("{}", s);
 }
 
 #[cfg(test)]
@@ -135,7 +120,6 @@ mod tests {
         let input = ".#.#.#\n...##.\n#....#\n..#...\n#.#..#\n####..\n";
         assert_eq!(y15d18(input, 4, 1), 4);
         assert_eq!(y15d18(input, 5, 2), 17);
-        // assert_eq!(y15d18(input, 2, 2), 17);
     }
 
     #[test]
