0c194f2a11f3a1552af73362258fa5c417e83b86
Mario Finelli <mario@finel.li>
2023-12-04 22:32:55 +0100

    Add write-up for day nineteen

--- src/y15d19.rs
+++ src/y15d19.rs
@@ -15,7 +15,11 @@
 
 //! Advent of Code 2015 Day 19: <https://adventofcode.com/2015/day/19>
 //!
-//! TODO
+//! Today's challenge was quite hard. It took me a while to even figure out
+//! exactly what I was supposed to be doing in the problem. Eventually, after
+//! reading the prompt several more times and looking at some solutions on the
+//! subreddit I was able to figure out enough to be able to implement my
+//! solution.
 
 use rand::seq::SliceRandom;
 use rand::thread_rng;
@@ -23,7 +27,17 @@ use std::collections::HashSet;
 
 /// The solution for part one of the day nineteen challenge.
 ///
-/// TODO
+/// As usual we take the problem input as a string and then start parsing it.
+/// The input contains all of the replacements followed by an empty line and
+/// then the molecule. So we parse the replacements until we get to the empty
+/// line and then the molecule is the final line. Once we have our replacements
+/// we loop through them and then we loop and for each of them we loop through
+/// the windows of the molecule of the size of the replacement so that we can
+/// see if we can do a replacement. If we can then we build a new molecule
+/// using the replacement and then add it to our tracking
+/// [`std::collections::HashSet`] (we're only supposed to find distinct
+/// molecules). Once we've checked all of the possibilities for all of the
+/// replacements we just return the length of our set.
 ///
 /// # Example
 /// ```rust
@@ -73,7 +87,28 @@ pub fn y15d19p1(input: &str) -> u32 {
 
 /// The solution for part two of the day nineteen challenge.
 ///
-/// TODO
+/// This was very challenging and I had to rely on inspiration from the
+/// subreddit to figure out exactly how to solve this problem. As in part one
+/// we start by parsing the problem input string to get our replacements and
+/// molecule. Then we start a loop that we run until we reach `e`. The strategy
+/// here is to work backwards from the end molecule to the start instead of
+/// vice-versa as we'd need to implement a breadth-first-search or similar
+/// which would quickly explode in how many checks that we need to do and how
+/// long that it would take. Then we loop through all of the replacements
+/// keeping track of how many times we're able to make a substitution. When we
+/// can't make any more substitutions for that replacement (new == original)
+/// then we move on to the next one. If, after processing all of the
+/// replacements, the length of our "new" molecule after the replacements is
+/// the same as the length of our "old" molecule before the replacements (i.e.,
+/// we weren't able to do any replacements) then we reset our molecule
+/// replacement string back to the original, reset our substitution counter,
+/// shuffle the replacements array, and try again. This is the insight that I
+/// was able to get from the subreddit, it seems that many people took a
+/// similar approach, but as I understand it, it is technically possible to get
+/// a non-optimal solution however, it seems that based on many peoples' inputs
+/// there is actually only one path from `e` to the molecule so it should be
+/// okay. Once we've reached `e` the loop ends and we can return the number of
+/// substitutions that we made.
 ///
 /// # Example
 /// ```rust
@@ -124,7 +159,8 @@ pub fn y15d19p2(input: &str) -> u32 {
     count
 }
 
-/// TODO
+/// This function simply returns a substring of the given size from the given
+/// starting point as exists in the standard library of many other languages.
 fn substring(string: &str, start: usize, end: usize) -> String {
     let s: String = string.chars().skip(start).take(end).collect();
     s
@@ -136,7 +172,11 @@ mod tests {
     use std::fs;
 
     #[test]
-    fn test_substring() {}
+    fn test_substring() {
+        let input = "ABCDE";
+        assert_eq!(substring(input, 1, 2), "BC");
+        assert_eq!(substring(input, 2, 2), "CD");
+    }
 
     #[test]
     fn it_works() {
