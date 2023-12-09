13dd6965c45426a668445cd71c77cb8c98cb64b9
Mario Finelli <mario@finel.li>
2023-12-09 09:16:49 +0100

    Add write-up for day nine

--- src/y23d09.rs
+++ src/y23d09.rs
@@ -15,36 +15,53 @@
 
 //! Advent of Code 2023 Day 9: <https://adventofcode.com/2023/day/9>
 //!
-//! TODO
+//! A very straightforward challenge today. Details about the approach and
+//! solution in the description for the solve function.
 
 /// The solution for the day nine challenge.
 ///
-/// TODO
+/// As usual we take the input as a string and an integer to denote the part.
+/// In part `1` we need to find the next number in the sequence and in part
+/// `2` we need to find the first number in the sequence. For each sequence
+/// (line) we split the input and parse the numbers. Then, we find the diffs
+/// between each of the numbers. If all of the diffs are zero then we're done
+/// finding the diffs. Otherwise we add the new list of diffs to a vector of
+/// diffs and then run the same process on that list continuing until we have
+/// all zeros. Then we calculate the previous and next numbers in the sequence.
+/// We reverse the list of diffs that we have and add the previous value (zero
+/// to start) to the next item that we pop from the beginning or the end of
+/// the diff that we're processing. When we're done we've found the next (or
+/// previous) number in the sequence and can add it to the vector tracking all
+/// of the sequences. Finally, based on the part we can return the sum of the
+/// next numbers or the previous numbers.
 ///
 /// # Example
 /// ```rust
 /// # use aoc::y23d09::y23d09;
 /// // probably read this from the input file...
-/// let input = "";
-/// assert_eq!(y23d09(input, 1), 1);
-/// assert_eq!(y23d09(input, 2), 1);
+/// let input = "9 12 17 23 28 40";
+/// assert_eq!(y23d09(input, 1), 88);
+/// assert_eq!(y23d09(input, 2), -3);
 /// ```
 pub fn y23d09(input: &str, part: u32) -> i32 {
-    let mut first_numbers = Vec::new();
+    let mut previous_numbers = Vec::new();
     let mut next_numbers = Vec::new();
 
     for line in input.lines() {
-        let numbers: Vec<i32> = line.split_whitespace().map(|n| n.parse().unwrap()).collect();
+        let numbers: Vec<i32> = line
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
 
-        let first_number = numbers[0];
-        let next_number = numbers[numbers.len() -1];
+        let previous_number = numbers[0];
+        let next_number = numbers[numbers.len() - 1];
         let mut check = numbers;
         let mut all_diffs = Vec::new();
         loop {
             let mut diffs = Vec::new();
 
             for pair in check.windows(2) {
-                diffs.push(pair[1]-pair[0]);
+                diffs.push(pair[1] - pair[0]);
             }
 
             if diffs.iter().all(|d| *d == 0) {
@@ -59,17 +76,17 @@ pub fn y23d09(input: &str, part: u32) -> i32 {
         let mut last_value = 0;
         for diff in all_diffs.iter().rev() {
             first_value = diff[0] - first_value;
-            last_value = diff.last().unwrap() + last_value;
+            last_value += diff[diff.len() - 1];
         }
 
         next_numbers.push(next_number + last_value);
-        first_numbers.push(first_number - first_value);
+        previous_numbers.push(previous_number - first_value);
     }
 
     if part == 1 {
         next_numbers.iter().sum()
     } else {
-        first_numbers.iter().sum()
+        previous_numbers.iter().sum()
     }
 }
 
